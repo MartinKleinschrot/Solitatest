256aa209351e9d7db31b18b2bf23ff2c93ad4e3c
Martin Kleinschrot <martin.kleinschrot@solita.fi>
2023-09-11 12:28:20 +0300

    added test.py

---
+++ test.py
@@ -0,0 +1,11 @@
+#Testfile
+
+def main():
+    name = input("Who are you?")
+    print("hello " + name)
+
+
+    
+# This name == main is just a best practise to use if you possibly plan to use the functions in this file in onther scripts.
+if __name__ == '__main__':
+    main()
